b73f2e6c92e7f74fbc61c9305deeea175d4e57f0
agakshita5 <akshita.ag2020@gmail.com>
2024-11-20 20:22:17 +0530

    added port

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import pickle
 import os
 from datetime import timedelta
 
-# port = int(os.environ.get('PORT', 5000))
+port = int(os.environ.get('PORT', 5000))
 app = Flask(__name__)
 app.secret_key = os.urandom(24) 
 app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes=30)  # Set session timeout duration
@@ -111,5 +111,5 @@ def result():
 
 
 if __name__ == "__main__":
-    # app.run(debug=True, host='0.0.0.0', port=port)
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=port)
+    # app.run(debug=True)
